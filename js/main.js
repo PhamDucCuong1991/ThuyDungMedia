88ad1b026a020909ca2f5ec2e4cacfa9f425fd1c
PhamDucCuong1991 <cuongvnb@gmail.com>
2023-11-01 17:44:58 +0700

    1

--- js/main.js
+++ js/main.js
@@ -80,16 +80,67 @@ function functionF() {
 
 }
 
+let isClicked = false; // Khởi tạo biến trạng thái ban đầu là false
+
+const checkbox = $("#remember-me");
+
+checkbox.on("click", function () {
+    isClicked = !isClicked;
+});
+
+let isLoggedIn = false;
+
 function login() {
+    isLoggedIn = true;
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const a = $('#remember-me');
+
     if (username === "admin" && password === "12345") {
         localStorage.setItem("loggedInUser", username);
         alert("Đăng nhập thành công!");
-        location.href = "../HTML/traCuu.html";
+        // location.href = "../HTML/traCuu.html";
+        $('#tm-hello').append(' ' + '( Xin chào sếp!)');
+        $('#idLogin').text('Logout');
+        $('#link-than-so').text('Logout');
+        if (isClicked) {
+            setTimeout(() => {
+                localStorage.clear();
+                $('#idLogin').text('Login');
+            }, 365* 24 * 60 * 60 * 1000)
+        } else {
+            setTimeout(() => {
+                localStorage.clear();
+                $('#idLogin').text('Login');
+            }, 24 * 60 * 60 * 1000)
+        }
+
     } else {
         alert("Tài khoản hoặc mật khẩu không chính xác!");
     }
     $("#username").val("");
     $("#password").val("");
+    a.prop('checked', false);
 }
+
+// Hàm xử lý khi đăng xuất
+function logout() {
+    isLoggedIn = false;
+    $('#idLogin').text('Login');
+    $('#tm-hello').empty()
+    localStorage.clear();
+}
+
+// Sự kiện khi người dùng nhấp vào thẻ <a>
+$('#idLogin').on('click', function (event) {
+    event.preventDefault(); // Ngăn chặn hành động mặc định của thẻ <a>
+    if (isLoggedIn) {
+        logout(); // Nếu đã đăng nhập, chuyển đến hàm logout
+    }
+});
+$('#link-than-so').on('click', function (event) {
+    event.preventDefault(); // Ngăn chặn hành động mặc định của thẻ <a>
+    if (isLoggedIn) {
+        logout(); // Nếu đã đăng nhập, chuyển đến hàm logout
+    }
+});
